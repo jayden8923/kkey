d17ecd2f88d75e68037b77266470e3bcbf15b6ab
jayden8923 <45021556+jayden8923@users.noreply.github.com>
2022-02-16 20:51:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,46 @@
 var timesPerSecond = 0;
 var rate = 0;
+var i = 15;
 var times = 0;
-var time = document.getElementById('settime');
-var settime = toNumber(time.value);
 var socket = io('https://kkey-leaderboard.herokuapp.com/');
 var countingDown = false;
 var myVar = setInterval(checkRate, 1000);
 var leaderboard = [];
-var i;
-
 window.onkeyup = function(e) {
-   var key = e.keyCode ? e.keyCode : e.which;
-
-   if (key == 75 && countingDown) {
-       onTimer();
-      }
-
-     times = times + 5000000000000000000000000000000;
-     timesPerSecond = timesPerSecond + 5000000000000000000000000000000;
-     document.getElementById('pressed').innerHTML = times;
-   }
+    var key = e.keyCode ? e.keyCode : e.which;
+    if (key == 75) {
+        if (!countingDown) {
+            i = 3;
+            countingDown = true;
+            onTimer();
+        }
+        times = times + 5000000000000000000000000000000;
+        timesPerSecond = timesPerSecond + 5000000000000000000000000000000;
+        document.getElementById('pressed').innerHTML = times;
+    }
 }
 
 function onTimer() {
-  document.getElementById('mycounter').innerHTML = settime;
-   countingDown = true;
-  settime--;
-  if (settime < 0) {
-    alert('TIME UP');
-    socket.emit('score',{player: prompt('What is your name?'), score: times})
-    countingDown = false;
-    times = 0;
-     time.value = 30;
-  }
+    document.getElementById('mycounter').innerHTML = i;
+    i--;
+    if (i < 0) {
+        alert('TIME UP');
+        socket.emit('score', {
+            player: prompt('What is your name?'),
+            score: times
+        }) countingDown = false;
+        times = 0;
+    } else if (countingDown) {
+        setTimeout(onTimer, 1000);
+    }
 }
 
 function checkRate() {
-   rate = timesPerSecond;
-   document.getElementById('rate').innerHTML = rate;
-   timesPerSecond = 0;
-}
-
-//socket stuff
-socket.on('connected', function () {
-  console.log('Successfully connected to server.');
-});
-
-socket.on('updateL', function(newLeader){
-  leaderboard = newLeader;
-  console.log(leaderboard);
-  var string = ''
-  for(var i = 0; i<leaderboard.length; i++){
-    string = string + "#" + eval(i+1) + " " + leaderboard[i].player + ": " + leaderboard[i].score + "<br>";
-  }
-  document.getElementById('leaderboard').innerHTML = '<strong>Leaderboard:</strong><br>' + string;
-})
-
-function clearLeaderboard(){
-  socket.emit('clear', password);
-}
+    rate = timesPerSecond;
+    document.getElementById('rate').innerHTML = rate;
+    timesPerSecond = 0;
+} //socket stuff 
+socket.on('connected', function () { console.log('Successfully connected to server.'); }); 
+socket.on('updateL', function(newLeader){ leaderboard = newLeader; console.log(leaderboard); var string = '' for(var i = 0; i<leaderboard.length; i++){ string = string + "#" + eval(i+1) + " " + leaderboard[i].player + ": " + leaderboard[i].score + "<br>"; }
+ document.getElementById('leaderboard').innerHTML = '<strong>Leaderboard:</strong><br>' + string; }) 
+function clearLeaderboard(){ socket.emit('clear', password); } 
